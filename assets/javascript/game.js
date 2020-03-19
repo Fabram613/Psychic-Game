6b72a06d61aad5abe0435204e697be262b635f14
Fabian <49567569+Fabram613@users.noreply.github.com>
2020-03-19 14:59:16 -0500

    fixed some css elements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,5 @@
-// The specific letters that the user typed.
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// Setting for zero
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
@@ -9,7 +7,7 @@ var letterUser = [];
 var eachofLetters = null;
 
 
-// Sets the computerGuess variable equal to a random choice from the computerChoice array.
+// Sets the computerGuess variable equal to a random choice from the computerChoices array:
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 function countGuessesLeft() {
@@ -28,7 +26,7 @@ var restart = function() {
 	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
 
-// When the user presses a key, it will run the following function..
+// When the user presses a key it will fire the following function:
 document.onkeyup = function(event) {
 	guessesLeft--;
 
